64d9536d9eadd0f2ba440a5ffd21d013e2b1d10a
Michael <michaelsmith1205@yahoo.com>
2018-08-08 19:27:11 -0400

    Testing App purposes

---
+++ resources/views/pages/about.blade.php
@@ -0,0 +1,7 @@
+@extends('layouts.app')
+
+@section('content')
+<H1>About</H1>
+<p>This is the about page</p>
+    @endsection
+    
---
+++ resources/views/pages/index.blade.php
@@ -0,0 +1,11 @@
+@extends('layouts.app')
+
+@section('content')
+    <div class="jumbotron text-center"
+    <H1>Welcome to  Laravelblog</H1>
+    <p>How are you doing today?</p>
+<p><a class="btn btn-primary btn-lg" href="/login" role="button">Login</a> <a class=" btn btn-lg" href="/register" role="button">Register</a></p>
+</div>
+    @endsection
+   
+    
